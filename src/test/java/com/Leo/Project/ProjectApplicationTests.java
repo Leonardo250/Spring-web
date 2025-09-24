40c7b6855adba426d1d562b9e43499277969a7a9
Leonardo250 <leomoulin2002@gmail.com>
2025-09-24 12:06:07 -0300

    Projeto Iniciado

---
+++ src/test/java/com/Leo/Project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Leo.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
